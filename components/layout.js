33e7517679dc9ebb67f9332adf93aca8945df907
technocalorie <findabirr@gmail.com>
2022-12-08 11:07:23 +0600

    Initial commit from Create Next App

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+import Header from "./header";
+import Footer from "./footer";
+import Meta from "./meta";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Meta />
+      <Header />
+      <div className='min-h-screen'>
+        <main>{children}</main>
+      </div>
+      <Footer />
+    </>
+  );
+}
